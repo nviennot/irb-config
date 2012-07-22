e854c8823b19f1f8515c65f0adfac1f748b9e9fa
Nicolas Viennot <nicolas@viennot.biz>
2012-07-21 22:41:11 -0400

    Colorized Logger: Support for mongoid 3

--- irb/mongoid_logger.rb
+++ irb/mongoid_logger.rb
@@ -11,17 +11,47 @@ module IRB
 
       old_formatter = Mongoid.logger.formatter
       Mongoid.logger.formatter = proc do |severity, datetime, progname, msg|
-        m = msg.match(/^MONGODB \(([^)]*)\) (.*)$/)
+        m = parse(msg)
         if m.nil?
           unless msg =~ /which could negatively impact client-side performance/
             old_formatter.call(severity, datetime, progname, msg)
           end
         else
-          duration = m[1]
-          command = m[2]
-          command.gsub!(/BSON::ObjectId\('([^']+)'\)/, '0x\1')
-          "\033[1;32m☘ \033[1;37mMongoDB\033[0m #{duration} #{CodeRay.scan(command, :ruby).term}\n"
+          m[:query].gsub!(/BSON::ObjectId\('([^']+)'\)/, '0x\1')
+          m[:duration] = m[:duration].split('.')[0] if m[:duration]
+
+          line = "\033[1;32m☘ \033[1;37mMongoDB\033[0m "
+          line << "(#{m[:duration]}ms) " if m[:duration]
+
+          if m[:database]
+            if m[:collection]
+              line << colorize("[#{m[:database]}::#{m[:collection]}] ")
+            else
+              line << colorize("[#{m[:database]}] ")
+            end
+          end
+
+          line << "#{colorize(m[:operation])} " if m[:operation]
+          line << colorize(m[:query])
+          line << "\n"
         end
+      end
+    end
+
+    def self.colorize(msg)
+      CodeRay.scan(msg, :ruby).term
+    end
+
+    def self.parse(msg)
+      case msg
+      when /^MONGODB \((.*)ms\) (.*)\['(.*)'\]\.(.*)$/
+        {:duration => $1, :database => $2, :collection => $3, :query => $4}
+      when /^MONGODB (.*)\['(.*)'\]\.(.*)$/
+        {:database => $1, :collection => $2, :query => $3}
+      when /^ *MOPED: (\S+:\S+) (\S+) +database=(\S+) collection=(\S+) (.*) \((.*)ms\)/
+        {:host => $1, :operation => $2, :database => $3, :collection => $4, :query => $5, :duration => $6}
+      when /^ *MOPED: (\S+:\S+) (\S+) +database=(\S+) (.*) \((.*)ms\)/
+        {:host => $1, :operation => $2, :database => $3, :query => $4, :duration => $5}
       end
     end
 
