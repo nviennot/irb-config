3106b3c7bf92f29fab114a74f495cc9d633f2167
Nicolas Viennot <nicolas@viennot.biz>
2013-04-17 21:43:29 -0400

    Cyan messages for Rails debug messages it looks better :)

--- irb/rails_colors.rb
+++ irb/rails_colors.rb
@@ -1,12 +1,12 @@
 module IRB
   module RailsLogger
-    # Purple messages for Rails logger
+    # Cyan messages for Rails logger
     def self.setup
       require 'logger'
       Rails.logger = Logger.new(STDERR)
-      Rails.logger.level = Logger::WARN
+      Rails.logger.level = Logger::INFO
       Rails.logger.formatter = proc do |severity, datetime, progname, msg|
-        "\033[35m#{msg}\033[0m\n"
+        "\033[36m#{msg}\033[0m\n"
       end
     rescue
     end
