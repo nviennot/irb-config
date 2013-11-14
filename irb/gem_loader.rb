75e9440cd197f41d3cf2f84202e924c5bf1bf01d
Nicolas Viennot <nicolas@viennot.biz>
2013-11-14 13:47:53 -0500

    Fix rbenv not found warning

--- irb/gem_loader.rb
+++ irb/gem_loader.rb
@@ -2,7 +2,7 @@ module IRB
   module GemLoader
     def self.setup
       # For rbenv, we load the gems from our special directory
-      irb_gemset = `rbenv which ruby`.chop.split('/')[0..-3].join('/') + "/lib/ruby/gems/irb-config" rescue nil
+      irb_gemset = `rbenv which ruby 2> /dev/null`.chop.split('/')[0..-3].join('/') + "/lib/ruby/gems/irb-config" rescue nil
       # On RVM, We make everything in the global gemset available, bypassing Bundler
       irb_gemset ||= ( ENV["GEM_PATH"] || `rvm $(rvm current) do gem env path`.chop ).split(':').grep(/ruby.*@global/).first rescue nil
       Dir["#{irb_gemset}/gems/*"].each { |path| $LOAD_PATH << "#{path}/lib" } if irb_gemset
