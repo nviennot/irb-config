0fcd54da47c77987d3ae02bc936605bab1ae13f6
Nicolas Viennot <nicolas@viennot.biz>
2013-11-14 13:57:58 -0500

    Extrated the logic to find our gems

--- irb/gem_loader.rb
+++ irb/gem_loader.rb
@@ -1,10 +1,16 @@
 module IRB
   module GemLoader
-    def self.setup
+    def self.irb_gemsetpath
       # For rbenv, we load the gems from our special directory
-      irb_gemset = `rbenv which ruby 2> /dev/null`.chop.split('/')[0..-3].join('/') + "/lib/ruby/gems/irb-config" rescue nil
+      path = `rbenv which ruby 2> /dev/null`.chop.split('/')[0..-3].join('/') rescue nil
+      return path + "/lib/ruby/gems/irb-config" if path.to_s.size > 0
+
       # On RVM, We make everything in the global gemset available, bypassing Bundler
-      irb_gemset ||= ( ENV["GEM_PATH"] || `rvm $(rvm current) do gem env path`.chop ).split(':').grep(/ruby.*@global/).first rescue nil
+      (ENV["GEM_PATH"] || `rvm $(rvm current) do gem env path`.chop).split(':').grep(/ruby.*@global/).first rescue nil
+    end
+
+    def self.setup
+      irb_gemset = irb_gemsetpath
       Dir["#{irb_gemset}/gems/*"].each { |path| $LOAD_PATH << "#{path}/lib" } if irb_gemset
     end
     setup
