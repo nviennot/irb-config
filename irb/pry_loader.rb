af9f41ae0cac2b50515d3b30fc8bd7baa78a8293
Nicolas Viennot <nicolas@viennot.biz>
2014-08-26 05:03:48 -0400

    Improved prompt

--- irb/pry_loader.rb
+++ irb/pry_loader.rb
@@ -6,9 +6,18 @@ module IRB
       load_pry_plugins
       trap_winchange
 
-      ::Pry.prompt = [proc { |obj, nest_level, pry| "#{self.pwd} (#{obj}) > " },
-                      proc { |obj, nest_level, pry| "#{self.pwd} (#{obj}) * " }]
+      ::Pry.prompt = [proc { |obj, nest_level, pry| get_prompt(obj, nest_level, pry, ">") },
+                      proc { |obj, nest_level, pry| get_prompt(obj, nest_level, pry, "|") }]
       @@home = Dir.home
+    end
+
+    def self.get_prompt(obj, nest_level, pry, suffix)
+      obj_str = obj.to_s
+      max_length = 20
+      if obj_str.size > max_length+3
+        obj_str = "#{obj_str[0, max_length/2]}...#{obj_str[-max_length/2..-1]}"
+      end
+      "#{self.pwd} (#{obj_str})#{":#{nest_level}" unless nest_level.zero?} #{suffix} "
     end
 
     def self.linux?
